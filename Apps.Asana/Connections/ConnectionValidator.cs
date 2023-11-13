a32aa43827324f785024ad26bc143d691c4f5dba
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-11-13 17:00:18 +0200

    Connection validator added

--- Apps.Asana/Connections/ConnectionValidator.cs
+++ Apps.Asana/Connections/ConnectionValidator.cs
@@ -1,6 +1,10 @@
 ﻿
+using Apps.Asana.Api;
+using Apps.Asana.Constants;
+using Apps.Asana.Dtos.Base;
 using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
+using Blackbird.Applications.Sdk.Utils.Extensions.String;
 using RestSharp;
 
 namespace Apps.Asana.Connections
@@ -10,7 +14,20 @@ namespace Apps.Asana.Connections
         public async ValueTask<ConnectionValidationResponse> ValidateConnection(
        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
         {
-            return new() { IsValid = true };
+            var client = new AsanaClient();
+            var endpoint = ApiEndpoints.Projects.WithQuery(new {});
+            var request = new AsanaRequest(endpoint, Method.Get, authProviders);
+
+            try
+            {
+                await client.ExecuteWithErrorHandling<IEnumerable<AsanaEntity>>(request);
+                return new() { IsValid = true, Message = "Success" };
+            }
+            catch (Exception ex)
+            {
+                return new() { IsValid = false, Message = ex.Message };
+            }
+            
         }
     }
 }
