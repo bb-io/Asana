adc1320189b3a1a50280afc8f43cb437ddd63690
bohdanm-bb <bohdan.makarenko@blackbird.io>
2024-08-15 15:37:56 +0300

    Debug

--- Apps.Asana/Connections/ConnectionValidator.cs
+++ Apps.Asana/Connections/ConnectionValidator.cs
@@ -14,19 +14,20 @@ namespace Apps.Asana.Connections
         public async ValueTask<ConnectionValidationResponse> ValidateConnection(
        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
         {
-            var client = new AsanaClient();
-            var endpoint = ApiEndpoints.Projects.WithQuery(new {});
-            var request = new AsanaRequest(endpoint, Method.Get, authProviders);
+            return new() { IsValid = true, Message = "Success" };
+            //var client = new AsanaClient();
+            //var endpoint = ApiEndpoints.Projects.WithQuery(new {});
+            //var request = new AsanaRequest(endpoint, Method.Get, authProviders);
 
-            try
-            {
-                await client.ExecuteWithErrorHandling<IEnumerable<AsanaEntity>>(request);
-                return new() { IsValid = true, Message = "Success" };
-            }
-            catch (Exception ex)
-            {
-                return new() { IsValid = false, Message = ex.Message };
-            }
+            //try
+            //{
+            //    await client.ExecuteWithErrorHandling<IEnumerable<AsanaEntity>>(request);
+            //    return new() { IsValid = true, Message = "Success" };
+            //}
+            //catch (Exception ex)
+            //{
+            //    return new() { IsValid = false, Message = ex.Message };
+            //}
             
         }
     }
