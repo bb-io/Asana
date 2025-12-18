22d2837fc8285c4fe67962e15b4a06dfa9c2c71c
Artem Riabushenko <ariabushenko@blackbird.io>
2025-12-18 16:31:01 +0200

    Adjustments to action 'Update task'

---
+++ Apps.Asana/Models/Tasks/Requests/TaskSectionRequest.cs
@@ -0,0 +1,13 @@
+﻿using Apps.Asana.DataSourceHandlers;
+using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Dynamic;
+
+namespace Apps.Asana.Models.Tasks.Requests
+{
+    public class TaskSectionRequest
+    {
+        [Display("Section ID")]
+        [DataSource(typeof(SectionDataHandler))]
+        public string? SectionId { get; set; }
+    }
+}
