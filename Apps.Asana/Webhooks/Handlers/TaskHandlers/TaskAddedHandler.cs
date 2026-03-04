630673d7f9483929212f55835f0d7fd78791e01a
Artem Riabushenko <ariabushenko@blackbird.io>
2026-03-04 23:52:38 +0200

    Webhook adjustments to retrive workspace

--- Apps.Asana/Webhooks/Handlers/TaskHandlers/TaskAddedHandler.cs
+++ Apps.Asana/Webhooks/Handlers/TaskHandlers/TaskAddedHandler.cs
@@ -4,7 +4,7 @@ using Blackbird.Applications.Sdk.Common.Webhooks;
 namespace Apps.Asana.Webhooks.Handlers.TaskHandlers;
 
 public class TaskAddedHandler([WebhookParameter(true)] ProjectRequest r)
-    : BaseWebhookHandler(r.ProjectId, ResourceType, Action, r.WorkspaceId)
+    : BaseWebhookHandler(r.ProjectId, ResourceType, Action, workspaceId: r.WorkspaceId)
 {
     const string ResourceType = "task";
     const string Action = "added";
--- Apps.Asana/Webhooks/Handlers/TaskHandlers/TaskChangedHandler.cs
+++ Apps.Asana/Webhooks/Handlers/TaskHandlers/TaskChangedHandler.cs
@@ -4,7 +4,7 @@ using Blackbird.Applications.Sdk.Common.Webhooks;
 namespace Apps.Asana.Webhooks.Handlers.TaskHandlers;
 
 public class TaskChangedHandler([WebhookParameter(true)] TaskRequest tr)
-    : BaseWebhookHandler(tr.TaskId, ResourceType, Action, tr.WorkspaceId)
+    : BaseWebhookHandler(tr.TaskId, ResourceType, Action, workspaceId: tr.WorkspaceId)
 {
     const string ResourceType = "task";
     const string Action = "changed";
