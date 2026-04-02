b73042fa4a0c36d3b70eaada4f577e25ec29b07b
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-04-02 11:37:55 +0300

    move custom fields filtering to client side for the 'On tasks changed' event

--- Apps.Asana/Models/Tasks/Requests/TaskCustomFieldsRequest.cs
+++ Apps.Asana/Models/Tasks/Requests/TaskCustomFieldsRequest.cs
@@ -1,11 +1,10 @@
 ﻿using Apps.Asana.DataSourceHandlers;
-using Apps.Asana.Models.Tasks.Requests;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
-namespace Apps.Asana.Webhooks.Models.Request;
+namespace Apps.Asana.Models.Tasks.Requests;
 
-public class OnTasksChangedRequest : TaskRequest
+public class TaskCustomFieldsRequest
 {
     [Display("Custom fields"), DataSource(typeof(TaskCustomFieldsDataHandler))]
     public IEnumerable<string>? CustomFieldIds { get; set; }
--- Apps.Asana/Webhooks/Handlers/TaskHandlers/TaskChangedHandler.cs
+++ Apps.Asana/Webhooks/Handlers/TaskHandlers/TaskChangedHandler.cs
@@ -1,39 +1,11 @@
-﻿using Apps.Asana.Webhooks.Models.Request;
+﻿using Apps.Asana.Models.Tasks.Requests;
 using Blackbird.Applications.Sdk.Common.Webhooks;
 
 namespace Apps.Asana.Webhooks.Handlers.TaskHandlers;
 
-public class TaskChangedHandler([WebhookParameter(true)] OnTasksChangedRequest tr) 
+public class TaskChangedHandler([WebhookParameter(true)] TaskRequest tr) 
     : BaseWebhookHandler(tr.TaskId, ResourceType, Action, workspaceId: tr.WorkspaceId)
 {
     const string ResourceType = "task";
     const string Action = "changed";
-
-    protected override Dictionary<string, object> BuildFilter()
-    {
-        var filter = base.BuildFilter();
-        if (tr.CustomFieldIds != null && tr.CustomFieldIds.Any())
-            filter["fields"] = new[] { "custom_fields" };
-
-        return filter;
-    }
-
-    protected override bool FilterEquals(Dictionary<string, object> a, Dictionary<string, object> b)
-    {
-        if (!base.FilterEquals(a, b)) 
-            return false;
-
-        var aHasFields = a.TryGetValue("fields", out var aFields);
-        var bHasFields = b.TryGetValue("fields", out var bFields);
-
-        if (!aHasFields && !bHasFields) 
-            return true;
-        if (aHasFields != bHasFields) 
-            return false;
-
-        var aArray = ((IEnumerable<object>)aFields!).Select(x => x.ToString()).OrderBy(x => x);
-        var bArray = ((IEnumerable<object>)bFields!).Select(x => x.ToString()).OrderBy(x => x);
-
-        return aArray.SequenceEqual(bArray);
-    }
 }
