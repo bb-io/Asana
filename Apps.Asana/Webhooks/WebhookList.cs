037e7a6891207ca93e02f23d0b35457172ff806d
Fesenko-A <111450737+Fesenko-A@users.noreply.github.com>
2026-09-18 11:42:14 +0200

    Revert "add webhook logger"
    This reverts commit 71961f4ce882aadc2565a9a60f0bdac9d4486529.

--- Apps.Asana/Webhooks/WebhookList.cs
+++ Apps.Asana/Webhooks/WebhookList.cs
@@ -45,7 +45,6 @@ public class WebhookList(InvocationContext invocationContext) : BaseInvocable(in
 {
     public Task<HttpResponseMessage?> HandleHandshakeAsync(WebhookRequest request)
     {
-        WebhookLogger.Log($"HandleHandshakeAsync fired. Request body: {JsonConvert.SerializeObject(request)}");
         const string secretHeaderKey = "X-Hook-Secret";
         
         if (!request.TryGetHookSecret(secretHeaderKey, out var secretKey))
@@ -62,7 +61,6 @@ public class WebhookList(InvocationContext invocationContext) : BaseInvocable(in
             Content = new StringContent(string.Empty)
         };
         response.Headers.Add(secretHeaderKey, secretKey);
-        WebhookLogger.Log($"HandleHandshakeAsync response body: {JsonConvert.SerializeObject(response)}");
         
         return Task.FromResult<HttpResponseMessage?>(response);
     }
